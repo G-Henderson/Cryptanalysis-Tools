f9e613b98218daa00d4a40bb395659fd56f9d3bc
LAPTOP <admin@xeiverse.co.uk>
2022-11-16 20:36:21 +0000

    Updates

---
+++ utils/solvers/railfence_solver.py
@@ -0,0 +1,57 @@
+from utils.cipher_solver import CipherSolver
+
+class RailfenceSolver(CipherSolver):
+
+    """
+    Class for decrypting ciphertexts encrypted using the Caesar cipher
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def with_key(self, message: str, key: int) -> str:
+        if (key == 1):
+            return message
+        else:
+            pt = [""for i in range(len(message))]
+            k=0
+            for line in range(key-1):
+                skip=2*(key-line-1)
+                j=0
+                i=line
+                while (i<len(message)):
+                    pt[i] = message[k]
+                    k+=1
+                    if ((line==0) or (j%2 == 0)):
+                        i+=skip
+                    else:
+                        i+=2*(key-1) - skip  
+                    j+=1       
+
+            i=line+1  
+            while (i<len(message)):
+                pt[i] = message[k]
+                k+=1
+                i+=2*(key-1)
+
+            output = "".join(pt)
+
+            return output
+
+    def brute_force(self, message: str) -> str:
+        best_score = -99e9
+        best_msg = ""
+        for key in range(1,26):
+            # Get the shifted message
+            curr_msg = self.with_key(message, key)
+
+            # Get a score for that message
+            curr_score = self.quadgram_scorer.ngram_score(curr_msg)
+
+            # Check if score is better than previous best
+            if (curr_score > best_score):
+                # Update the best score and best message
+                best_score = curr_score
+                best_msg = curr_msg
+
+        return best_msg
---
+++ utils/solvers/transposition_solver.py
@@ -0,0 +1,137 @@
+from utils.cipher_solver import CipherSolver
+
+class TranspositionSolver(CipherSolver):
+
+    """
+    Class for decrypting ciphertexts encrypted using the Transposition cipher
+    """
+
+    def __init__(self) -> None:
+        super().__init__()
+
+    def with_key(self, message: str, key: str) -> str:
+        # Setup grid
+        grid = self.create_grid(message, key)
+
+        # Put key into list
+        key_as_list = list(key.upper())
+
+        # Put key into alphabetical order
+        sorted_list = sorted(key_as_list)
+
+        output = ""
+        for i in range(len(grid)):
+            for j in range(len(grid[i])):
+                output += grid[i][sorted_list.index(key_as_list[j])]
+
+        # Return the rearranged message
+        return output
+
+    def with_keylen(self, message: str, keylen: int) -> str:
+        # Get the number of digits needed for the key
+        my_digits = [str(i) for i in range(keylen)]
+
+        # Get the permutations of those digits
+        my_permutations = self.get_permutations(my_digits)
+
+        # Create variables to store the highest score and best permutation
+        best_score = -99e9
+        best_permutation = ""
+
+        # Try all of the permutations
+        for perm in my_permutations:
+            # Convert the permutation to a string
+            current_permutation = "".join(perm)
+            # Decrypt using that permutation
+            decrypt = self.with_key(message, current_permutation)
+
+            # Score the decryption
+            current_score = self.quadgram_scorer.ngram_score(decrypt)
+
+            # Check if it is a new best score
+            if (current_score > best_score):
+                # Replace the best score and best permutation
+                best_score = current_score
+                best_permutation = current_permutation
+
+        # Decrypt the message using the best permutation
+        decrypt = self.with_key(message, best_permutation)
+
+        # Return the decrypted message
+        return decrypt
+
+    def brute_force(self, message: str) -> str:
+        # Setup empty variable to store the best scoring message
+        best_message = ""
+        # Setup really low score as benchmark
+        best_score = -99e9
+        # Insert a new line
+        print("")
+
+        # Iterate through key lengths between 2 and 9
+        for keylen in range(2,10):
+            # Get the best message using that keylen
+            decrypt = self.with_keylen(message, keylen)
+            # Get the score for the new decrypt
+            current_score = self.quadgram_scorer.ngram_score(decrypt)
+
+            # Print message to show progress
+            print(f"Key length: {keylen}, Score: {current_score}, Decrypt: {decrypt[:30]}...")
+
+            # See if we have a new best score
+            if (current_score > best_score):
+                # Set the new best score and the new best message
+                best_score = current_score
+                best_message = decrypt
+
+        # Return the best message
+        return best_message
+
+    def create_grid(self, message: str, key: str) -> list:
+        # Put key into list
+        key_as_list = list(key.upper())
+
+        # Put key into alphabetical order
+        sorted_list = sorted(key_as_list)
+
+        # Get the number of columns and rows
+        num_cols = len(key)
+        num_rows = len(message) // num_cols
+
+        # Create grid
+        grid = [[""for y in range(num_cols)]for x in range(num_rows)]
+
+        for i in range(len(message)):
+            pass
+        
+        # Return the grid variable
+        return grid
+
+    def get_permutations(self, list_digits: list) -> list:
+        """
+        Function to find all permutations of a string (that has been converted to a list)
+        """
+
+        # Check that there are digits to find permutations of
+        if (len(list_digits) == 0):
+            return []
+
+        # Check whether only 1 permutation is possible
+        elif (len(list_digits) == 1):
+            return [list_digits]
+
+        # Else find the permutations of the digits given
+        else:
+            # Create an empty list to store the permutations
+            my_permutations = []
+
+            # Iterate through the list
+            for digit in list_digits:
+                other_digits = list_digits[:list_digits.index(digit)] + list_digits[list_digits.index(digit)+1:]
+
+                # Get all the permutations where the current digit is 1st
+                for permutation in self.get_permutations(other_digits):
+                    my_permutations.append([digit] + permutation)
+
+            # Return the list of permutations
+            return my_permutations
